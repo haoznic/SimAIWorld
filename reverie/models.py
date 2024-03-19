2657ecb0aded4a17b2751e725eb6a9887246e8c9
星舰 <xingjian.zh@alibaba-inc.com>
2024-03-19 16:12:46 +0800

    123

--- reverie/models.py
+++ reverie/models.py
@@ -10,6 +10,10 @@ os.environ["OPENAI_API_KEY"] = "openai key"
 os.environ["COHERE_API_KEY"] = "cohere key"
 os.environ["ANTHROPIC_API_KEY"] = "anthropic key"
 
+os.environ["DASHSCOPE_API_KEY"] = "your api"
+os.environ["ALIBABA_CLOUD_ACCESS_KEY_ID"] = "your api"
+os.environ["ALIBABA_CLOUD_ACCESS_KEY_SECRET"] = "your api"
+
 messages = [{"content": "Hello, how are you?", "role": "user"}]
 
 # openai call
